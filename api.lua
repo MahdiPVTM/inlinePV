5103889b363dcf695e5515d4908b1552988c8641
MahdiPVTM <30749461+MahdiPVTM@users.noreply.github.com>
2017-09-01 01:45:02 -0700

    Update api.lua

--- api.lua
+++ api.lua
@@ -1,12 +1,12 @@
-bot = dofile('/home/administrator/inlinePV/utils.lua')
-json = dofile('/home/administrator/inlinePV/JSON.lua')
+bot = dofile('/home/username/inlinePV/utils.lua')
+json = dofile('/home/username/inlinePV/JSON.lua')
 URL = require "socket.url"
 serpent = require("serpent")
 http = require "socket.http"
 https = require "ssl.https"
 redis = require('redis')
 db = redis.connect('127.0.0.1', 6379)
-BASE = '/home/administrator/inlinePV/'
+BASE = '/home/username/inlinePV/'
 SUDO = 372897257 -- sudo id
 sudo_users = {372897257,Userid}
 BOTS = 249464384 -- bot id
